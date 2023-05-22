2cd080dbe2e0ecc160a6ba2790a8a98a66ab51fc
Sean Gray <seangray.dev@gmail.com>
2023-05-22 19:29:37 -0400

    add selectors.js for helper functions

---
+++ src/helpers/selectors.js
@@ -0,0 +1,9 @@
+export function getAppointmentsForDay(state, day) {
+  const selectedDay = state.days.find((d) => d.name === day);
+
+  if (!selectedDay) {
+    return [];
+  }
+
+  return selectedDay.appointments.map((id) => state.appointments[id]);
+}
